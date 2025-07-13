770a6e9e66b44eea83c312794c47ad96377648af
Seth Burleson <sethbcoding@gmail.com>
2025-07-13 12:04:49 -0400

    add winnermessage to model

--- Snorehammer.Web/FrontendModels/FightSimulation.cs
+++ Snorehammer.Web/FrontendModels/FightSimulation.cs
@@ -6,5 +6,6 @@
         public List<Dice> StrengthDice { get; set; } = new List<Dice>();
         public List<Dice> ArmorDice { get; set; } = new List<Dice>();
         public List<Dice> WoundDice { get; set; } = new List<Dice>();
+        public string WinnerMessage { get; set; } = "";
     }
 }
