6f39ead154b021f4f1fb76d67bf6e3138fa0f92b
Seth Burleson <sethbcoding@gmail.com>
2025-07-16 16:47:52 -0400

    add plus properties

--- Snorehammer.Web/FrontendModels/AttackProfile.cs
+++ Snorehammer.Web/FrontendModels/AttackProfile.cs
@@ -22,6 +22,8 @@
         public bool Torrent { get; set; } = false;
         public bool Blast { get; set; } = false;
         public bool IsVariableAttacks { get; set; } = false;
+        public bool Plus1Hit { get; set; } = false;
+        public bool Plus1Wound { get; set; } = false;
         public int VariableAttackDiceNumber { get; set; }
         public int VariableAttackDiceSides { get; set; } = 6;
         public int VariableAttackDiceConstant { get; set; }
