46bb42a66b233b26f0901f60b6282825e6c3099b
Seth A Burleson <Sbrevolution5@aim.com>
2025-07-21 14:03:14 -0400

    Story/27 compare attackers (#110)
    * rename unitcompare to unitfight
    
    * reorganize files and create comparison
    
    * add model for comparison, rename comparison page to be distinct
    
    * add toggle switch to compare units
    
    ---------
    
    Co-authored-by: Seth Burleson <sethbcoding@gmail.com>

---
+++ Snorehammer.Web/FrontendModels/Comparison.cs
@@ -0,0 +1,13 @@
+﻿namespace Snorehammer.Web.FrontendModels
+{
+    public class Comparison
+    {
+        public bool Comparing { get; set; }
+        public AttackProfile Attacker1 { get; set; }
+        public AttackProfile Attacker2 { get; set; }
+        public UnitProfile Defender1 { get; set; }
+        //public UnitProfile Defender2 { get; set; }
+        public FightSimulation simulation1 { get; set; }
+        public FightSimulation simulation2 { get; set; }
+    }
+}
