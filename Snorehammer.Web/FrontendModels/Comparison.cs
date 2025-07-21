4ab59eb4ba96181bb1fa3e7b8718cc959d02ca4d
Seth Burleson <sethbcoding@gmail.com>
2025-07-21 14:28:39 -0400

    remove attacker1 and defender 1 now uses simulations

--- Snorehammer.Web/FrontendModels/Comparison.cs
+++ Snorehammer.Web/FrontendModels/Comparison.cs
@@ -4,10 +4,6 @@
     {
         public bool ComparingAttackers { get; set; }
         public bool ComparingDefenders { get; set; }
-        public AttackProfile Attacker1 { get; set; }
-        public AttackProfile Attacker2 { get; set; }
-        public UnitProfile Defender1 { get; set; }
-        public UnitProfile Defender2 { get; set; }
         public FightSimulation Simulation1 { get; set; }
         public FightSimulation AlternateAttackSimulation { get; set; }
         public FightSimulation AlternateDefenseSimulation { get; set; }
