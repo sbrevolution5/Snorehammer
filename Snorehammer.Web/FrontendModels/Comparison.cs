6deb0e5ce6913b97c2f61b0b83d2ce9ac0fbc750
Seth Burleson <sethbcoding@gmail.com>
2025-07-21 13:35:45 -0400

    add model for comparison, rename comparison page to be distinct

---
+++ Snorehammer.Web/FrontendModels/Comparison.cs
@@ -0,0 +1,12 @@
+﻿namespace Snorehammer.Web.FrontendModels
+{
+    public class Comparison
+    {
+        public AttackProfile Attacker1 { get; set; }
+        public AttackProfile Attacker2 { get; set; }
+        public UnitProfile Defender1 { get; set; }
+        //public UnitProfile Defender2 { get; set; }
+        public FightSimulation simulation1 { get; set; }
+        public FightSimulation simulation2 { get; set; }
+    }
+}
