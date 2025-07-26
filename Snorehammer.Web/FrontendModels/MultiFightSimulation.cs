4ca0d5ea7d3c7f6c00fbc859cd6a318013689908
Seth Burleson <sethbcoding@gmail.com>
2025-07-26 14:56:02 -0400

    add methods to simulate multiple fights and get averages

--- Snorehammer.Web/FrontendModels/MultiFightSimulation.cs
+++ Snorehammer.Web/FrontendModels/MultiFightSimulation.cs
@@ -6,13 +6,13 @@
         public UnitProfile Defender { get; set; }
         public List<FightSimulation> FightSimulations { get; set; } = new List<FightSimulation>();
         public int Rounds { get; set; }
-        public int AttackNumber { get; set; } = 0;
-        public int DamageNumber { get; set; } = 0;
-        public int ModelsDestroyed { get; set; } = 0;
-        public int WoundsInflicted { get; set; } = 0;
-        public int ArmorSavesFailed { get; set; } = 0;
-        public int AttacksHit { get; set; } = 0;
-        public int FeelNoPainMade { get; set; } = 0;
+        public float AttackNumber { get; set; } = 0;
+        public float DamageNumber { get; set; } = 0;
+        public float ModelsDestroyed { get; set; } = 0;
+        public float WoundsInflicted { get; set; } = 0;
+        public float ArmorSavesFailed { get; set; } = 0;
+        public float AttacksHit { get; set; } = 0;
+        public float FeelNoPainMade { get; set; } = 0;
         public void SetSimNumber(int number)
         {
             for (int i = 0; i < number; i++)
@@ -23,6 +23,16 @@
                     Defender = Defender,
                 });
             }
+        }
+        public void SetAverages()
+        {
+            AttackNumber = (float)FightSimulations.Select(f => f.AttackNumber).Average();
+            DamageNumber = (float)FightSimulations.Select(f => f.DamageNumber).Average();
+            ModelsDestroyed = (float)FightSimulations.Select(f => f.ModelsDestroyed).Average();
+            WoundsInflicted = (float)FightSimulations.Select(f => f.WoundsInflicted).Average();
+            ArmorSavesFailed = (float)FightSimulations.Select(f => f.ArmorSavesFailed).Average();
+            AttacksHit = (float)FightSimulations.Select(f => f.AttacksHit).Average();
+            FeelNoPainMade = (float)FightSimulations.Select(f => f.FeelNoPainMade).Average();
         }
     }
 }
