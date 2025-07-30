564c0b0449f264ebcfea816eff03d7090465d7e1
Seth Burleson <sethbcoding@gmail.com>
2025-07-30 11:32:21 -0400

    add new properties to sub-simulations

--- Snorehammer.Web/FrontendModels/Simulations/FightSimulation.cs
+++ Snorehammer.Web/FrontendModels/Simulations/FightSimulation.cs
@@ -24,7 +24,6 @@ namespace Snorehammer.Web.FrontendModels.Simulations
         public string WinnerMessage { get; set; } = "";
         public List<WeaponSimulation> WeaponSimulations { get; set; } = new List<WeaponSimulation>();
         public FightStats Stats { get; set; } = new FightStats();
-        public List<FightStats> WeaponStats { get; set; } = new List<FightStats>();
         public void Reset() {
             AttackDice = new List<Dice>();
             ArmorDice = new List<Dice>();
--- Snorehammer.Web/FrontendModels/Simulations/WeaponSimulation.cs
+++ Snorehammer.Web/FrontendModels/Simulations/WeaponSimulation.cs
@@ -1,6 +1,27 @@
-﻿namespace Snorehammer.Web.FrontendModels.Simulations
+﻿using Snorehammer.Web.FrontendModels.Profiles;
+using Snorehammer.Web.FrontendModels.Stats;
+
+namespace Snorehammer.Web.FrontendModels.Simulations
 {
     public class WeaponSimulation
     {
+        public AttackProfile Weapon { get; set; }
+        public UnitProfile Defender { get; set; }
+        public List<Dice> AttackDice { get; set; } = new List<Dice>();
+        public List<Dice> ToHitDice { get; set; } = new List<Dice>();
+        public List<Dice> StrengthDice { get; set; } = new List<Dice>();
+        public List<Dice> ArmorDice { get; set; } = new List<Dice>();
+        public List<Dice> WoundDice { get; set; } = new List<Dice>();
+        public List<Dice> FeelNoPainDice { get; set; } = new List<Dice>();
+        public int HitTarget = 0;
+        public int WoundTarget = 0;
+        public int ModdedWoundTarget = 0;
+        public int ArmorSave { get; set; } = 0;
+        public int AttackNumber { get; set; } = 0;
+        public int DamageNumber { get; set; } = 0;
+        public int BlastBonus = 0;
+        public bool CoverIgnored { get; set; } = false;
+        public string WinnerMessage { get; set; } = "";
+        public FightStats Stats { get; set; } = new FightStats();
     }
 }
