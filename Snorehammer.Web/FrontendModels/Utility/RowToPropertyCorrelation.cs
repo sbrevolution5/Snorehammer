20c650943d26c357d799e723f78c86fb1b7b39f7
Seth Burleson <sethbcoding@gmail.com>
2025-08-01 15:38:14 -0400

    datagrid now converts into rows

---
+++ Snorehammer.Web/FrontendModels/Utility/RowToPropertyCorrelation.cs
@@ -0,0 +1,13 @@
+﻿namespace Snorehammer.Web.FrontendModels.Utility
+{
+    public class RowToPropertyCorrelation
+    {
+        public string RowName { get; set; }
+        public string PropertyName { get; set; }
+        public RowToPropertyCorrelation(string rowName, string propertyName)
+        {
+            RowName = rowName;
+            PropertyName = propertyName;
+        }
+    }
+}
