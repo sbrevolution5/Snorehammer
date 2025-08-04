b44ee7a193b54ec04629323d58b5097c96e1daa3
Seth A Burleson <Sbrevolution5@aim.com>
2025-08-04 13:11:03 -0400

    97 graphs (#194)
    * add basic graph
    
    * refactor graph to its own object
    
    * add pie graphs
    
    * put pie graphs in columns
    
    * add tabbed display for bar graphs
    
    * add multiple pie graphs
    
    * add pie tabs
    
    * remove bugged per-weapon stats table
    
    ---------
    
    Co-authored-by: Seth Burleson <sethbcoding@gmail.com>

---
+++ Snorehammer.Web/FrontendModels/Stats/BarGraphStat.cs
@@ -0,0 +1,8 @@
+﻿namespace Snorehammer.Web.FrontendModels.Stats
+{
+    public class BarGraphStat
+    {
+        public int Category { get; set; }
+        public int Value { get; set; }
+    }
+}
---
+++ Snorehammer.Web/Services/GraphCreationService.cs
@@ -0,0 +1,40 @@
+﻿using Snorehammer.Web.FrontendModels.Stats;
+
+namespace Snorehammer.Web.Services
+{
+    public class GraphCreationService
+    {
+        public List<BarGraphStat> MakeBarGraphStats(List<int> values)
+        {
+            var res = new List<BarGraphStat>();
+            var possibleResults = values.Distinct();
+            foreach (var v in possibleResults)
+            {
+                res.Add(new BarGraphStat()
+                {
+                    Category = v,
+                    Value = values.Where(i=>i == v).Count()
+                });
+            }
+            res = res.OrderBy(s => s.Category).ToList();
+            return res;
+        }
+        public List<PieGraphStat> MakePieGraphStats(float value,int total)
+        {
+            var res = new List<PieGraphStat>
+            {
+                new PieGraphStat()
+                {
+                    Category = "Success",
+                    Value = value
+                },
+                new PieGraphStat()
+                {
+                    Category = "Failure",
+                    Value = (float)(total - value)
+                }
+            };
+            return res;
+        }
+    }
+}
