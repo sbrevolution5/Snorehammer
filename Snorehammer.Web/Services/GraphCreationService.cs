47e9f86a1bfabd04cc3a548b36c8f21d199e4dee
Seth Burleson <sethbcoding@gmail.com>
2025-08-04 12:20:05 -0400

    refactor graph to its own object

---
+++ Snorehammer.Web/Services/GraphCreationService.cs
@@ -0,0 +1,23 @@
+﻿using Snorehammer.Web.FrontendModels.Stats;
+
+namespace Snorehammer.Web.Services
+{
+    public class GraphCreationService
+    {
+        public List<GraphStat> MakeGraphStats(List<int> values)
+        {
+            var res = new List<GraphStat>();
+            var possibleResults = values.Distinct();
+            foreach (var v in possibleResults)
+            {
+                res.Add(new GraphStat()
+                {
+                    Category = v,
+                    Value = values.Where(i=>i == v).Count()
+                });
+            }
+            res = res.OrderBy(s => s.Category).ToList();
+            return res;
+        }
+    }
+}
