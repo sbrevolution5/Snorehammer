4ff64cbf9ee2faca6d1e98a90125877e2dd87bc7
Seth A Burleson <Sbrevolution5@aim.com>
2025-08-11 15:27:45 -0400

    207 sub menus (#210)
    * add panel for anti-X
    
    * factor out into summary service
    
    * add hitsummary()
    
    * put sustained and lethal in hit roll
    
    * add wound section, blast goes with hits
    
    * add wound and damage panels
    
    * add panels to unit profile input
    
    * call result of armorsummary not just method
    
    * put attack inputs in columns that are collapsible
    
    * add ability summaries to panels
    
    * start submenus already collapsed
    
    ---------
    
    Co-authored-by: Seth Burleson <sethbcoding@gmail.com>

---
+++ Snorehammer.Web/Services/SummaryService.cs
@@ -0,0 +1,336 @@
+﻿using Snorehammer.Web.FrontendModels.Profiles;
+using System.Text;
+
+namespace Snorehammer.Web.Services
+{
+    public class SummaryService
+    {
+        public string HitSummary(UnitProfile unit)
+        {
+            var sb = new StringBuilder();
+            var first = true;
+            if (unit.Stealth)
+            {
+                sb.Append("Stealth");
+                first = false;
+            }
+            if (unit.Minus1Hit)
+            {
+                if (!first)
+                {
+                    sb.Append(" | ");
+                }
+                sb.Append("-1 to hit");
+            }
+            var res = sb.ToString();
+            if (string.IsNullOrEmpty(res))
+            {
+                return "no modifiers";
+            }
+            return res;
+        }
+        public string HitSummary(AttackProfile attack)
+        {
+            var sb = new StringBuilder();
+            var first = true;
+            if (attack.RerollHit)
+            {
+                sb.Append("Reroll all hits");
+                first = false;
+            }
+            if (attack.Plus1Hit)
+            {
+                if (!first)
+                {
+                    sb.Append(" | ");
+                }
+                sb.Append("+1 to hit");
+                first = false;
+            }
+            if (attack.Minus1Hit)
+            {
+                if (!first)
+                {
+                    sb.Append(" | ");
+                }
+                sb.Append("-1 to hit");
+                first = false;
+            }
+
+            if (attack.Torrent && !attack.Melee)
+            {
+                if (!first)
+                {
+                    sb.Append(" | ");
+                }
+                sb.Append("Torrent");
+                first = false;
+            }
+            if (attack.Reroll1Hit)
+            {
+                if (!first)
+                {
+                    sb.Append(" | ");
+                }
+                sb.Append("Reroll 1s to hit");
+                first = false;
+            }
+            if (attack.BigGuns)
+            {
+                if (!first)
+                {
+                    sb.Append(" | ");
+                }
+                sb.Append("Big Guns (-1)");
+                first = false;
+            }
+            if (attack.Sustained)
+            {
+                if (!first)
+                {
+                    sb.Append(" | ");
+                }
+                sb.Append($"Sustained {attack.SustainAmount}");
+                first = false;
+            }
+            if (attack.Blast)
+            {
+                if (!first)
+                {
+                    sb.Append(" | ");
+                }
+                sb.Append("Blast");
+                first = false;
+            }
+            if (attack.Lethal)
+            {
+                if (!first)
+                {
+                    sb.Append(" | ");
+                }
+                sb.Append("Lethal Hits");
+            }
+            var res = sb.ToString();
+            if (string.IsNullOrEmpty(res))
+            {
+                return "no modifiers";
+            }
+            return res;
+        }
+        public string WoundSummary(UnitProfile unit)
+        {
+            var sb = new StringBuilder();
+            var first = true;
+            if (unit.Minus1Wound)
+            {
+                sb.Append("-1 to wound");
+                first = false;
+            }
+            if (unit.MinusOneToWoundAgainstStronger)
+            {
+                if (!first)
+                {
+                    sb.Append(" | ");
+                }
+                sb.Append("-1 to Wound when Strength > toughness");
+            }
+            var res = sb.ToString();
+            if (string.IsNullOrEmpty(res))
+            {
+                return "no modifiers";
+            }
+            return res;
+        }
+        public string WoundSummary(AttackProfile attack)
+        {
+            var sb = new StringBuilder();
+            var first = true;
+            if (attack.RerollWound)
+            {
+                sb.Append("Twin-Linked/Reroll all wounds");
+                first = false;
+            }
+            if (attack.Plus1Wound)
+            {
+                if (!first)
+                {
+                    sb.Append(" | ");
+                }
+                sb.Append("+1 to Wound");
+                first = false;
+            }
+
+            if (attack.Lance && attack.Melee)
+            {
+                if (!first)
+                {
+                    sb.Append(" | ");
+                }
+                sb.Append("Lance");
+                first = false;
+            }
+            if (attack.Devastating)
+            {
+                if (!first)
+                {
+                    sb.Append(" | ");
+                }
+                sb.Append("Devastating Wounds");
+                first = false;
+            }
+            if (attack.Reroll1Wound)
+            {
+                if (!first)
+                {
+                    sb.Append(" | ");
+                }
+                sb.Append("Reroll 1s to Wound");
+            }
+            var res = sb.ToString();
+            if (string.IsNullOrEmpty(res))
+            {
+                return "no modifiers";
+            }
+            return res;
+        }
+        public string ArmorSummary(UnitProfile unit)
+        {
+            var sb = new StringBuilder();
+            var first = true;
+            if (unit.ArmorReroll)
+            {
+                sb.Append("Reroll Armor Saves");
+                first = false;
+            }
+            if (unit.Reroll1Save)
+            {
+                if (!first)
+                {
+                    sb.Append(" | ");
+                }
+                sb.Append("Reroll 1s to Save");
+                first = false;
+            }
+            if (unit.HasCover)
+            {
+                if (!first)
+                {
+                    sb.Append(" | ");
+                }
+                sb.Append("Cover");
+                first = false;
+            }
+            var res = sb.ToString();
+            if (string.IsNullOrEmpty(res))
+            {
+                return "no modifiers";
+            }
+            return res;
+        }
+        public string DamageSummary(AttackProfile attack)
+        {
+            var sb = new StringBuilder();
+            var first = true;
+            if (attack.RerollDamage)
+            {
+                sb.Append("Reroll all damage");
+                first = false;
+            }
+            if (attack.Reroll1Damage)
+            {
+                if (!first)
+                {
+                    sb.Append(" | ");
+                }
+                sb.Append("Reroll 1s for damage");
+                first = false;
+            }
+
+            if (attack.Melta)
+            {
+                if (!first)
+                {
+                    sb.Append(" | ");
+                }
+                sb.Append($"Melta {attack.MeltaDamage}");
+            }
+            var res = sb.ToString();
+            if (string.IsNullOrEmpty(res))
+            {
+                return "no modifiers";
+            }
+            return res;
+        }
+        public string AntiSummary(AttackProfile attack)
+        {
+            var sb = new StringBuilder();
+            if (attack.AntiBeast)
+            {
+                sb.Append($"Anti-Beast {attack.AntiBeastValue}  ");
+            }
+            if (attack.AntiInfantry)
+            {
+                sb.Append($"Anti-Infantry {attack.AntiInfantryValue}  ");
+            }
+            if (attack.AntiMonster)
+            {
+                sb.Append($"Anti-Monster {attack.AntiMonsterValue}  ");
+            }
+            if (attack.AntiVehicle)
+            {
+                sb.Append($"Anti-Vehicle {attack.AntiVehicleValue}  ");
+            }
+            if (attack.AntiMounted)
+            {
+                sb.Append($"Anti-Mounted {attack.AntiMountedValue}  ");
+            }
+            if (attack.AntiSwarm)
+            {
+                sb.Append($"Anti-Swarm {attack.AntiSwarmValue}  ");
+            }
+            var res = sb.ToString();
+            if (string.IsNullOrEmpty(res))
+            {
+                return "no Anti-";
+            }
+            return res;
+        }
+        public string AbilitySummary(AttackProfile attack) {
+            var sb = new StringBuilder();
+            var hit = HitSummary(attack);
+            if (hit != "no modifiers") {
+                sb.AppendLine(hit);
+            }
+            var wound = WoundSummary(attack);
+            if (wound != "no modifiers") {
+                sb.AppendLine(wound);
+            }
+            var damage = DamageSummary(attack);
+            if (damage != "no modifiers") {
+                sb.AppendLine(damage);
+            }
+            var anti = AntiSummary(attack);
+            if (anti != "no Anti-")
+            {
+                sb.AppendLine(anti);
+            }
+            return sb.ToString();
+        }
+        public string AbilitySummary(UnitProfile unit) {
+            var sb = new StringBuilder();
+            var hit = HitSummary(unit);
+            if (hit != "no modifiers") {
+                sb.AppendLine(hit);
+            }
+            var wound = WoundSummary(unit);
+            if (wound != "no modifiers") {
+                sb.AppendLine(wound);
+            }
+            var damage = ArmorSummary(unit);
+            if (damage != "no modifiers") {
+                sb.AppendLine(damage);
+            }
+            return sb.ToString();
+        }
+    }
+}
