b1793b9314e868ab5dc1bd0010f64445eab7cb0f
Seth Burleson <sethbcoding@gmail.com>
2025-08-11 14:50:21 -0400

    factor out into summary service

---
+++ Snorehammer.Web/Services/SummaryService.cs
@@ -0,0 +1,105 @@
+﻿using Snorehammer.Web.FrontendModels.Profiles;
+using System.Text;
+
+namespace Snorehammer.Web.Services
+{
+    public class SummaryService
+    {
+        public string HitSummary(AttackProfile attack)
+        {
+            var sb = new StringBuilder();
+            var first = true;
+            if (attack.RerollHit)
+            {
+                sb.Append("Reroll all hits");
+                first = false;
+            }
+            if (attack.Plus1Hit)
+            {
+                if (!first)
+                {
+                    sb.Append(" | ");
+                }
+                sb.Append("+1 to hit");
+                first = false;
+            }
+            if (attack.Minus1Hit)
+            {
+                if (!first)
+                {
+                    sb.Append(" | ");
+                }
+                sb.Append("-1 to hit");
+                first = false;
+            }
+            
+            if (attack.Torrent && !attack.Melee)
+            {
+                if (!first)
+                {
+                    sb.Append(" | ");
+                }
+                sb.Append("Torrent");
+                first = false;
+            }
+            if (attack.Reroll1Hit)
+            {
+                if (!first)
+                {
+                    sb.Append(" | ");
+                }
+                sb.Append("Reroll 1s to hit");
+                first = false;
+            }
+            if (attack.BigGuns)
+            {
+                if (!first)
+                {
+                    sb.Append(" | ");
+                }
+                sb.Append("Big Guns (-1)");
+                first = false;
+            }
+            var res = sb.ToString();
+            if (string.IsNullOrEmpty(res))
+            {
+                return "no modifiers";
+            }
+            return res;
+        }
+        public string AntiSummary(AttackProfile attack)
+        {
+            var sb = new StringBuilder();
+            if (attack.AntiBeast)
+            {
+                sb.Append($"Anti-Beast {attack.AntiBeastValue}  ");
+            }
+            if (attack.AntiInfantry)
+            {
+                sb.Append($"Anti-Infantry {attack.AntiInfantryValue}  ");
+            }
+            if (attack.AntiMonster)
+            {
+                sb.Append($"Anti-Monster {attack.AntiMonsterValue}  ");
+            }
+            if (attack.AntiVehicle)
+            {
+                sb.Append($"Anti-Vehicle {attack.AntiVehicleValue}  ");
+            }
+            if (attack.AntiMounted)
+            {
+                sb.Append($"Anti-Mounted {attack.AntiMountedValue}  ");
+            }
+            if (attack.AntiSwarm)
+            {
+                sb.Append($"Anti-Swarm {attack.AntiSwarmValue}  ");
+            }
+            var res = sb.ToString();
+            if (string.IsNullOrEmpty(res))
+            {
+                return "no Anti-";
+            }
+            return res;
+        }
+    }
+}
