f1db25e82ccef5d521754dc6c04032f917defcaa
Seth Burleson <sethbcoding@gmail.com>
2025-08-12 13:54:12 -0400

    add property

--- Snorehammer.Web/FrontendModels/Profiles/UnitProfile.cs
+++ Snorehammer.Web/FrontendModels/Profiles/UnitProfile.cs
@@ -26,6 +26,7 @@ namespace Snorehammer.Web.FrontendModels.Profiles
         public int FeelNoPainTarget { get; set; } = 6;
         public bool HasCover { get; set; } = false;
         public bool MinusOneToWoundAgainstStronger { get; set; } = false;
+        public bool TakesHalfDamage { get; set; } = false;
         public object Clone()
         {
             UnitProfile res = (UnitProfile)this.MemberwiseClone();
