4031c6483ffacc43c02cd98eadf59e2dd7712da5
Seth Burleson <sethbcoding@gmail.com>
2025-08-13 14:30:25 -0400

    factor out parameters into graph data model

---
+++ Snorehammer.Web/FrontendModels/Stats/GraphData.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace Snorehammer.Web.FrontendModels.Stats
+{
+    public class GraphData
+    {
+        public List<FightStats> dataA { get; set; }
+        public List<FightStats> dataB { get; set; }
+        public int TotalSims { get; set; }
+        public MultiFightStats multiA { get; set; }
+        public MultiFightStats? multiB { get; set; }
+        public string titleA { get; set; }
+        public string titleB { get; set; }
+        public bool showFnp { get; set; } = false;
+        public bool isMultiAttack { get; set; } = false;
+    }
+}
